8131a6436e83f499ba94343288773b1a0a6ecb75
AnttiV <antti.virta@gmail.com>
2011-09-07 16:48:27 +0300

    Removed old file (was a txt, thanks windows) First implementation of PID.

---
+++ libraries/PID/PID.h
@@ -0,0 +1,63 @@
+﻿#ifndef PID_h
+#define PID_h
+
+#include <stdlib.h>
+#include "WProgram.h"
+#include "pins_arduino.h"
+
+class PID {
+	public:
+	
+	void initialize(float p, float i, float d, float ref, float in, float maxCtrl, float minCtrl) {
+		Kp = p;
+		Ki = i;
+		Kd = d;
+		prevTime = millis();
+		prevRef = ref;
+		prevInput = in;
+		
+		minLimit = minCtrl;
+		maxLimit = maxCtrl;
+	}
+
+	float calculate(float ref, float input) {
+		// Calculate sampling time
+		long dt = 0.001 * (millis() - prevTime); // Convert to seconds
+
+		
+		float error = ref - input;
+		pTerm = Kp * (ref - input);
+		dTerm = -Kd * (input - prevInput)/dt; // dError/dt = - dInput/dt
+		iTerm += Ki * error * dt;
+		
+		// Calculate control
+		float output = pTerm + iTerm + dTerm;
+		
+		// Anti-windup
+		if (output > maxLimit) {
+			iTerm -= output - maxLimit;
+			output = maxLimit;
+		} else {
+			iTerm += minLimit - output;
+			output = minLimit;
+		}
+		
+		// Update state
+		prevTime = millis();
+		prevRef = ref;
+		prevInput = input;
+		
+		return output;
+	}
+	
+	private:
+	long prevTime;
+	float prevRef;
+	float prevInput;
+	float pTerm;
+	float iTerm;
+	float dTerm;
+	float minLimit;
+	float maxLimit;
+	float Kp, Ki, Kd;
+};
